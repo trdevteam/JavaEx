fe776d7bc3c1eea4b4734fbdc90cfa71d0e8e6a1
trdevteam <tarikmurat.islam@gmail.com>
2016-11-23 09:05:42 +0200

    first commit

---
+++ Tutorial/src/DesignPatterns/Structural/Composite/Drawing.java
@@ -0,0 +1,33 @@
+package DesignPatterns.Structural.Composite;
+
+import java.util.ArrayList;
+
+public class Drawing implements Shape{
+
+	//collection of Shapes
+	private ArrayList<Shape> shapes = new ArrayList<Shape>();
+	
+	@Override
+	public void draw(String fillColor) {
+		for(Shape sh : shapes)
+		{
+			sh.draw(fillColor);
+		}
+	}
+	
+	//adding shape to drawing
+	public void add(Shape s){
+		this.shapes.add(s);
+	}
+	
+	//removing shape from drawing
+	public void remove(Shape s){
+		shapes.remove(s);
+	}
+	
+	//removing all the shapes
+	public void clear(){
+		System.out.println("Clearing all the shapes from drawing");
+		this.shapes.clear();
+	}
+}
